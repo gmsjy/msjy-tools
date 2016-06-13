de9e7b215bed7c45b6e50e289302cb41788b39df
gmsjy <ZTXYYCX@hotmail.com>
2016-06-13 12:28:18 +0800

    step 1

--- logstash_redis.py
+++ logstash_redis.py
@@ -4,7 +4,7 @@ import redis
 
 class CRedis:
 
-    def __init__(self, host="localhost", port=6379,db=0, channel="loghstash"):
+    def __init__(self, host="localhost", port=6379,db=0):
         ''' define the productor '''
         self.host = host
         self.port = port
@@ -12,4 +12,10 @@ class CRedis:
         self.channel = channel
         self.conn = redis.Redis(self.host, self.port)
 
-    def setChannel()
+    def setChannel(self, channel):
+        ''' Set the publish channel '''
+        self.channel = channel
+        return self 
+
+    def publishMsg(self, message):
+        '''publish the message'''
