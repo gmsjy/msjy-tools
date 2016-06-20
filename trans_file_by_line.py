bc4f7095c83309c52b42d4efea04d7360bdd14e6
gmsjy <ZTXYYCX@hotmail.com>
2016-06-20 20:48:40 +0800

    tcp server

--- trans_file_by_line.py
+++ trans_file_by_line.py
@@ -2,7 +2,7 @@
 # deliver the file by the lines
 
 
-import socket
+from socket import *
 import os
 import time
 from multiprocessing import Pool
@@ -18,10 +18,10 @@ filename = '/home/vagrant/web_log/http.log'
 
 class send_file(object):
     ''' class for the file send '''
-    def __init__(self, addr, log_file):
+    def __init__(self, addr, log_file, send_sock):
         self.addr = addr
         self.log_file = log_file
-        self.send_sock = socket.socket(AF_INET, SOCK_STREAM)
+        self.send_sock = send_sock
         self.line_num = 1
 
     def loging(self, info):
@@ -30,7 +30,7 @@ class send_file(object):
             info = '\n' + currentTime+' '+info
             f.write(info)
 
-    def send(self, log_filename, source_ip, log_file):
+    def send(self, log_filename, source_ip):
         self.send_sock.connect(self.addr)
         try:
             with open(filename,'rb') as f:
@@ -51,7 +51,8 @@ class send_file(object):
 
 
 if __name__ == "__main__":
-    node3 = send_file(ADDR,logfile)
+    send_sock = socket(AF_INET, SOCK_STREAM)
+    node3 = send_file(ADDR,logfile,send_sock)
     node3.send(filename,ip)
 
 
